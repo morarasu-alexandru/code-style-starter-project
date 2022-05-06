12d4cc7ca93509e462ea15a05e348e4d1f2a7061
Alexandru Morarasu <alexandru-florian.morarasu@stefanini.com>
2022-05-06 17:50:18 +0300

    conflict test

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(<App />);
+root.render(<App />); // comment here to have conflict
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
